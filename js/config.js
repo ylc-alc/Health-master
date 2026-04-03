032ff22dc568cfa3eab155ce18383b32f926d154
alc-learner <157918946+ylc-alc@users.noreply.github.com>
2026-04-03 13:48:04 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,8 @@
+export const STORAGE_KEY = 'personalisedWorkoutPlan_v8';
 
+export const DEFAULT_PROGRESS = {
+  completed: {},
+  easierMode: false,
+  dayState: 'standard',
+  audioEnabled: true
+};
