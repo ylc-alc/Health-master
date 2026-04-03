a42ae4595e6d8269c3f4b3f418d63f9fa3d4a698
alc-learner <157918946+ylc-alc@users.noreply.github.com>
2026-04-03 14:14:50 +0100

    Update player-timing.js

--- js/player/player-timing.js
+++ js/player/player-timing.js
@@ -1,1 +1,9 @@
+export function stopTimer(ctx) {
+  clearInterval(ctx.state.intervalId);
+  ctx.state.intervalId = null;
+}
 
+export function startTimer(ctx, tickFn) {
+  stopTimer(ctx);
+  ctx.state.intervalId = setInterval(tickFn, 1000);
+}
