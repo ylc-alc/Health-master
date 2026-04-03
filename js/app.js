255c4e695120a7120bece4f28f0c239b8e3d408c
alc-learner <157918946+ylc-alc@users.noreply.github.com>
2026-04-03 13:01:20 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
+import { initState } from './state.js';
+import { bindProgrammeActions } from './actions/programme-actions.js';
+import { bindOverlayActions } from './actions/overlay-actions.js';
+import { bindImportExport } from './actions/import-export.js';
+import { renderAll } from './render/index.js';
 
+function initApp() {
+  initState();
+  bindProgrammeActions();
+  bindOverlayActions();
+  bindImportExport();
+  renderAll();
+}
+
+initApp();
